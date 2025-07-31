aca716912c0ba3fdd9fce65438150ffa7afe923b
arsahak <arsahak99@gmail.com>
2025-07-31 12:14:04 +0600

    first commit

---
+++ config/index.js
@@ -0,0 +1,14 @@
+// Old Image Upload Configuration Cloudinary
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const CLOUDINARY_CLOUD_NAME = "dq80tmgg9";
+export const CLOUDINARY_UPLOAD_PRESET = "blogdata";
+export const CLOUDINARY_FOLDER = "blogdata";
+
+// New Image Upload Configuration Digital Ocean
+
+export const DIGITALOCEAN_ENDPOINT = "https://nyc3.digitaloceanspaces.com";
+export const DIGITALOCEAN_API_KEY = "DO00HFNWVPYJBRTBVZZJ";
+export const DIGITALOCEAN_SECRET_ACCESS_KEY =
+  "VTaTd4LASdw+OjDylmiBOPa4GOD956R/25597C7ULX8";
+export const DIGITALOCEAN_FOLDER = "sabbir-nasir";
